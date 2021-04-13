7a29d24a64c40172d441990bc861e29a6737d82d
Vadim M <vadimmukovozov@gmail.com>
2021-04-13 12:59:46 +0300

    init

---
+++ frontend/src/components/Messages/Messages.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Messages.css";
+import Message from "../Message/Message";
+
+const Messages = ({ messages, name }) => {
+  return (
+    <div className="messages">
+      {messages.map((message, i) => (
+        <div key={i}>
+          <Message message={message} name={name} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Messages;
---
+++ frontend/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './TextContainer.css';
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    {
+      users
+        ? (
+          <div>
+            <h1>People in room:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
